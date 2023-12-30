54d0d54688121ad299bd8aecb7b7941f57014209
Oleg Medvedev <meddway@gmail.com>
2023-12-30 20:38:36 +0600

    Добавлены кнопки по редактированию и удалению

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -21,6 +21,14 @@ const TransactionList: React.FC = () => {
     void fetchData();
   }, []);
 
+  const handleDelete = async (id: string) => {
+    console.log('Deleting transaction with ID:', id);
+  };
+
+  const handleEdit = (id: string) => {
+        console.log('Editing transaction with ID:', id);
+  };
+
   if (!transactions || transactions.length === 0) {
     return <div className="alert alert-info">No transactions yet.</div>;
   }
@@ -28,16 +36,22 @@ const TransactionList: React.FC = () => {
   return (
     <div className="container mt-4">
       <h3>Transaction List</h3>
-      <div className="list-group mt-3">
-        {transactions.map((transaction) => (
-          <div key={transaction.id} className="list-group-item">
+      {transactions.map((transaction) => (
+        <div key={transaction.id} className="list-group mt-3">
+          <div className="list-group-item">
             <div>Category: {transaction.category}</div>
             <div>Amount: {transaction.amount} KGS</div>
             <div>Created At: {dayjs(transaction.createdAt).format('DD.MM.YYYY HH:mm:ss')}</div>
             <div>Type: {transaction.type}</div>
+            <button className="btn btn-danger" onClick={() => handleDelete(transaction.id)}>
+              Delete
+            </button>
+            <button className="btn btn-primary ms-3" onClick={() => handleEdit(transaction.id)}>
+              Edit
+            </button>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
     </div>
   );
 };
